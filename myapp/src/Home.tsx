e60cf0ed695b4a9d20f6b51f013e905437122f1f
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-09-09 22:30:07 +0530

    chasdasanges for

---
+++ myapp/src/Home.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import data, { IEmployee } from './data';
+
+export const Home = () => {
+
+   const [counter,setCounter] =  useState<number>(0)
+
+    const inc =(e:any)=>{
+
+       setCounter(counter+1);
+    }
+  return (
+    <div>{counter}
+    <button onClick={inc}>+</button>
+
+    {
+        data.map((v:IEmployee)=>{
+            return(<div>{v.Id} {v.Name}  </div>)
+        })
+    }
+    
+    </div>
+  )
+}
---
+++ myapp/src/data.ts
@@ -0,0 +1,19 @@
+const data : IEmployee[] = [
+    {
+        Id:1,
+        Name: "test"
+    },
+    {
+        Id:1,
+        Name:"Test2"
+    }
+];
+
+
+export interface  IEmployee
+{
+    Id: Number,
+    Name:string
+}
+
+export default data;
